5d135490d590eb4c55f793439e8580bb6ded2f09
iaabo <insbarroso@gmail.com>
2021-02-08 13:13:30 +0000

    create de app skelethon

---
+++ src/components/EditProduct.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EditProduct = () => {
+    return (
+        <div>
+            <h1>EditProduct</h1>
+        </div>
+    )
+}
+
+export default EditProduct
---
+++ src/components/ProductList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Product from "./Product";
+
+const ProductList = () => {
+  return (
+    <div>
+      <h1>ProductList</h1>
+      <Product />
+    </div>
+  );
+};
+
+export default ProductList;
