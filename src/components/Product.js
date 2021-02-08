05030d402c48db0e1bc937b051d07f4fafc87a39
iaabo <insbarroso@gmail.com>
2021-02-08 22:23:38 +0000

    render the products through a table

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,12 +1,27 @@
 import React from "react";
+import { Table } from "react-bootstrap";
 
 const Product = (props) => {
   return (
     <div>
-      <p>{props.title}</p>
-      <p>{props.category}</p>
-      <p>{props.price} €</p>
-      <img src={props.image} alt="" style={{ height: "40px", width: "40px" }} />
+      <Table>
+        <tr>
+          <td style={{ width: "20px" }}>{props.id}</td>
+          <td style={{ width: "200px" }}>{props.title}</td>
+          <td style={{ width: "100px" }}>{props.category}</td>
+          <td style={{ width: "100px" }}>{props.price} €</td>
+          <td style={{ width: "100px" }}>
+            <img
+              src={props.image}
+              alt=""
+              style={{ height: "40px", width: "40px" }}
+            />
+          </td>
+          <td style={{ width: "50px" }}>Check</td>
+          <td style={{ width: "50px" }}>Update</td>
+          <td style={{ width: "50px" }}>Eliminate</td>
+        </tr>
+      </Table>
     </div>
   );
 };
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Table, Button } from "react-bootstrap";
 import Product from "./Product";
 
 const ProductList = () => {
@@ -12,9 +13,27 @@ const ProductList = () => {
 
   return (
     <div>
-       {products.map((product) => (
-        <Product {...product} key={product.id} />
-      ))}
+      <div>
+        <h1>Store Inventory</h1>
+        <Button variant="secondary">Add Product</Button>
+      </div>
+      <div className="container">
+        <Table>
+          <tr>
+            <th style={{ width: "20px" }}>#</th>
+            <th style={{ width: "200px" }}>Title</th>
+            <th style={{ width: "100px" }}>Category</th>
+            <th style={{ width: "100px" }}>Price</th>
+            <th style={{ width: "100px" }}>Image</th>
+            <th style={{ width: "50px" }}>Check</th>
+            <th style={{ width: "50px" }}>Update</th>
+            <th style={{ width: "50px" }}>Eliminate</th>
+          </tr>
+        </Table>
+        {products.map((product) => (
+          <Product {...product} key={product.id} />
+        ))}
+      </div>
     </div>
   );
 };
